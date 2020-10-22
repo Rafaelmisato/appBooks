bb735a8b0e44f0ad8fb46105f6e5a6fd37367036
Rafael Misato <misatorafael@gmail.com>
2020-10-22 14:48:44 -0300

    Initial structure

---
+++ src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Logo, LogoTitle } from './styles';
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <Logo>B</Logo>
+      <LogoTitle>Books</LogoTitle>
+    </Container>
+  );
+};
+
+export default Dashboard;
---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.Text`
+  font-size: 100px;
+  color: #fff;
+  font-weight: bold;
+`;
+
+export const LogoTitle = styled.Text`
+  font-size: 22px;
+  margin-top: -20px;
+  color: #fff;
+`;
